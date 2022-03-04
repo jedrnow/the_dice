c5191966ca26ca00574cbeb2cff8c285d987e033
jinxxur <jnvqwerty@gmail.com>
2022-03-04 18:37:56 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+class Dice{
+    constructor(x){
+        this.x=x;
+    }
+
+    roll(){
+        this.x=Math.floor(Math.random()*(6-1))+1;        
+    }
+}
+the_dice = new Dice(1);
+
+
+document.querySelector("#mybutton").onmousedown = function(){
+    const diceimg = document.querySelector("#diceimg");
+    diceimg.src = "dice_roll.gif";
+}
+
+document.querySelector("#mybutton").onmouseup = function(){
+    const diceimg = document.querySelector("#diceimg");
+    the_dice.roll();
+    diceimg.src = `dice_${the_dice.x}.png`;
+}
