16f20597268ccc60492e003c47cedd13617a6fde
jinxxur <jnvqwerty@gmail.com>
2022-03-04 18:47:40 +0100

    done

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ the_dice = new Dice(1);
 
 document.querySelector("#mybutton").onmousedown = function(){
     const diceimg = document.querySelector("#diceimg");
-    diceimg.src = "dice_roll.gif";
+    diceimg.src = "img/dice_roll.gif";
 }
 
 document.querySelector("#mybutton").onmouseup = function(){
     const diceimg = document.querySelector("#diceimg");
     the_dice.roll();
-    diceimg.src = `dice_${the_dice.x}.png`;
+    diceimg.src = `img/dice_${the_dice.x}.png`;
 }
